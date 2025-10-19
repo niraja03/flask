6fe7830c4556531e3ca9625c270f607f959902f1
niraja <nirajakurra@gmail.com>
2025-10-19 16:16:36 +0530

    Add basic HTML login form to login1.py

---
+++ login1.py
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Form</title>
+</head>
+<body>
+    <h2>Login Form</h2>
+    <form action="/submit" method="POST">
+
+        <label>Username:</label>
+        <input type="text" name="username" required placeholder="enter username"><br><br>
+        <label>Password:</label>
+        <input type="password" name="password" required placeholder="enter password"><br><br>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
